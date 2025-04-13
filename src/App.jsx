30a7c4a5c1a64b36b6d20a03281240e82f210ed0
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-04-13 22:25:13 +0700

    Изменен URL API

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {useState} from "react";
 
 export default function App() {
     const [isVisible, setIsVisible] = useState(false);
-    const API_URL = 'http://127.0.0.1:8000/api';
+    const API_URL = 'https://southernlines.ru/api';
 
     return (
         <>
