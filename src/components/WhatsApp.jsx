95b97f3d22cc72fae09482acec722eee9effe120
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-04-15 01:08:02 +0700

    Исправлена ссылка на WhatsApp

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -2,7 +2,7 @@ import whatsapp from "../img/icons/whatsapp.svg";
 
 export default function WhatsApp() {
     return (
-        <a href="https://api.whatsapp.com/send?phone=+79235907071" target="_blank"
+        <a href="https://wa.me/79235907071" target="_blank"
            className="img-container" title="Напишите нам в WhatsApp">
             <img src={whatsapp || ''} alt="Ссылка на WhatsApp для связи"
                  title="Напишите нам в WhatsApp"/>
